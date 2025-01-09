21667ac3ce996eb3c30b78065dc365fd7d609637
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-09 18:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+orderedList = document.getElementById("infi-list");
+for(let i = 1; i <= 10; i++){
+	orderedList.innerHTML += `
+		<li>Item${i}</li>
+	`;
+}
 
+
+orderedList.onscroll = ()=>{
+	let liCount = orderedList.children.length;
+	if(orderedList.scrollTop + orderedList.clientHeight >= orderedList.scrollHeight){
+		liCount++;
+		orderedList.innerHTML += `
+			<li>Item${liCount}</li>
+			<li>Item${liCount+1}</li>
+		`;
+	}
+	orderedList.innerHTML += `<li>Item${i}</li>`
+}
